179fa6f9dfe5de080a078c6c43345a28e705d762
Luiz Ferreira <luiferreira@br0c02dx6jamd6n.ml.com>
2021-05-20 11:17:32 -0300

    add dummy activity

---
+++ src/com/grupo4/Activity.java
@@ -0,0 +1,5 @@
+package com.grupo4;
+
+public interface Activity {
+    void execute();
+}
---
+++ src/com/grupo4/DummyActivity.java
@@ -0,0 +1,9 @@
+package com.grupo4;
+
+public class DummyActivity implements Activity {
+    @Override
+    public void execute() {
+        int i = 0;
+        System.out.println(i);
+    }
+}
