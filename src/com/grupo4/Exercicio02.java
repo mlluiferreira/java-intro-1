e837362c6f9bfcdd85e2a46e6ca72aa60b076047
Israel Melo <israel.melo@mercadolivre.com>
2021-05-20 11:41:52 -0300

    Added Exercicio02

---
+++ src/com/grupo4/Exercicio02.java
@@ -0,0 +1,20 @@
+package com.grupo4;
+
+import java.util.Scanner;
+
+public class Exercicio02 implements Activity {
+//    Desenvolva um programa para mostrar os primeiros n números múltiplos de m, sendo n e m valores que o
+//    usuário irá inserir via console. Lembre-se que um número a é divisível por b se a é divisível por b.
+    @Override
+    public void execute() {
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("Exercicio 02 - Digite um valor: ");
+        int numero = teclado.nextInt();
+        System.out.print("Exercicio 02 - Digite um numero para verificar o multiplo: ");
+        int multiplo = teclado.nextInt();
+
+        for (int i = 0 ; i < multiplo ; i ++){
+            if (i * multiplo == numero ) System.out.println(i);
+        }
+    }
+}
--- src/com/grupo4/MainApplication.java
+++ src/com/grupo4/MainApplication.java
@@ -2,6 +2,7 @@ package com.grupo4;
 
 public class MainApplication {
     public static void main(String... args) {
-
+        Exercicio02 exercicio02 = new Exercicio02();
+        exercicio02.execute();
     }
 }
