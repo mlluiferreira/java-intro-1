e4a8bd5fcc13e7b73fecd004e0069143c84cf596
Thiago Schnr <thiago.schnr@mercadolivre.com>
2021-05-20 11:47:07 -0300

    Add comentarios

--- src/com/grupo4/ExercicioUm.java
+++ src/com/grupo4/ExercicioUm.java
@@ -5,9 +5,12 @@ import java.util.Scanner;
 public class ExercicioUm implements Activity {
     @Override
     public void execute() {
+
+        //Desenvolva um programa para mostrar os primeiros n números pares, 
+        //sendo n um valor que o usuário irá inserir pelo console. 
         Scanner keyboard  = new Scanner(System.in);
 
-        System.out.println("Insira um valor:");
+        System.out.println("Exercicio 01 - Insira um valor:");
         int n = keyboard.nextInt();
 
         for (int i = 0; i < n; i++) {
