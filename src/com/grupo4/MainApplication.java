f6793719b4c390f69625509edbedd297bd559716
Israel Melo <israel.melo@mercadolivre.com>
2021-05-20 11:44:09 -0300

    Added Exercicio02

--- src/com/grupo4/MainApplication.java
+++ src/com/grupo4/MainApplication.java
@@ -2,7 +2,6 @@ package com.grupo4;
 
 public class MainApplication {
     public static void main(String... args) {
-        Exercicio02 exercicio02 = new Exercicio02();
-        exercicio02.execute();
+
     }
 }
