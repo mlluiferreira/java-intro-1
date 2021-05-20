9f066618d0309a26a8bc98f73578a2bca900eb36
Luiz Ferreira <luiferreira@br0c02dx6jamd6n.ml.com>
2021-05-20 12:58:23 -0300

    add main logic

--- src/com/grupo4/DummyActivity.java
+++ src/com/grupo4/DummyActivity.java
@@ -3,7 +3,6 @@ package com.grupo4;
 public class DummyActivity implements Activity {
     @Override
     public void execute() {
-        int i = 0;
-        System.out.println(i);
+        System.out.println("Exercicio nao existe");
     }
 }
--- src/com/grupo4/MainApplication.java
+++ src/com/grupo4/MainApplication.java
@@ -1,6 +1,28 @@
 package com.grupo4;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class MainApplication {
     public static void main(String... args) {
+
+        Map<Integer, Activity> activities = new HashMap<>();
+
+        activities.put(1, new ExercicioUm());
+        activities.put(2, new ExercicioDois());
+        activities.put(3, new Exercicio3());
+        activities.put(4, new ExercicioQuatro());
+        activities.put(5, new Exercicio5());
+
+        while(true) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Digite o numero do exercicio: ");
+            int exercicio = scanner.nextInt();
+
+            Activity atividade = activities.getOrDefault(exercicio, new DummyActivity());
+            atividade.execute();
+        }
+
     }
 }
